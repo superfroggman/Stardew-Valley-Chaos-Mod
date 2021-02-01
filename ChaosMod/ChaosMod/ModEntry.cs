67efcc23b98e497c99308b328bc1858753de56cd
superfroggman <49985901+superfroggman@users.noreply.github.com>
2021-02-01 16:57:22 +0100

    added functioning randomization of effects

--- ChaosMod/ChaosMod/ModEntry.cs
+++ ChaosMod/ChaosMod/ModEntry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
@@ -15,7 +16,8 @@ namespace ChaosMod
 
         public override void Entry(IModHelper helper)
         {
-            effects.Add(RandomEffect);
+            AddEffects();
+
             helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;
         }
 
@@ -25,31 +27,39 @@ namespace ChaosMod
             effects.Add(LowSpeed);
         }
 
-
+        int ticksPassed = 0;
         private void OnUpdateTicked(object sender, UpdateTickedEventArgs e)
         {
             if (!Context.IsPlayerFree || !Context.IsWorldReady || Game1.paused
                 || Game1.activeClickableMenu != null)
                 return;
 
-            RandomEffect();
+            ticksPassed += 1;
+
+            if(ticksPassed >= 600)
+            {
+                ticksPassed = 0;
+                RandomEffect();
+            }
         }
 
         static Random rnd = new Random();
         private void RandomEffect()
         {
             int r = rnd.Next(effects.Count);
-            effects[r]();
+            effects.ElementAt(r)();
         }
 
         private void HighSpeed()
         {
             Game1.player.addedSpeed = 10;
+            this.Monitor.Log("High Speed", LogLevel.Debug);
         }
 
         private void LowSpeed()
         {
-            Game1.player.addedSpeed = 0.2;
+            Game1.player.addedSpeed = 1;
+            this.Monitor.Log("Low Speed", LogLevel.Debug);
         }
     }
 }
